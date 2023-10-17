e017c3fb592d9fbeff50b809befa86191d3c1e85
danik1341 <danik1341@gmail.com>
2023-10-17 13:16:23 +0300

    remade fetching data with sort func

--- sanity/utils/sanity-utils.ts
+++ sanity/utils/sanity-utils.ts
@@ -1,13 +1,13 @@
-import {Project} from '../type/project'
-import client from '../../lib/sanity-client'
 import {groq} from 'next-sanity'
-import {Skill} from '../type/skill'
+
+import client from '../../lib/sanity-client'
 import {Experience} from '../type/experience'
+import {Project} from '../type/project'
+import {Skill} from '../type/skill'
 
 export async function getProjects(): Promise<Project[]> {
   return client.fetch(
-    groq`*[_type == "project"]{
-            _id,
+    groq`*[_type == "project"][0].projectsArray[]{
             title,
             description,
             tags,
@@ -20,11 +20,26 @@ export async function getProjects(): Promise<Project[]> {
         }`,
   )
 }
+// export async function getProjects(): Promise<Project[]> {
+//   return client.fetch(
+//     groq`*[_type == "project"]{
+//             _id,
+//             title,
+//             description,
+//             tags,
+//             "images": images[]{
+//                 "url": asset->url,
+//             },
+//             "slug": slug.current,
+//             githubLink,
+//             demoLink
+//         }`,
+//   )
+// }
 
 export async function getProject(slug: string): Promise<Project> {
   return client.fetch(
-    groq`*[_type == "project" && slug.current == $slug][0]{
-            _id,
+    groq`*[_type == "project"][0].projectsArray[slug.current == $slug][0]{
             title,
             description,
             tags,
@@ -34,29 +49,45 @@ export async function getProject(slug: string): Promise<Project> {
             "slug": slug.current,
             githubLink,
             demoLink
-        }`,
+    }`,
     {slug},
   )
 }
+
+// export async function getProject(slug: string): Promise<Project> {
+//   return client.fetch(
+//     groq`*[_type == "project" && slug.current == $slug][0]{
+//             _id,
+//             title,
+//             description,
+//             tags,
+//             "images": images[]{
+//                 "url": asset->url,
+//             },
+//             "slug": slug.current,
+//             githubLink,
+//             demoLink
+//         }`,
+//     {slug},
+//   )
+// }
 
 export async function getSkills(): Promise<Skill[] | null> {
   return client.fetch(
     groq`*[_type == "skills"][0].skills[]->{
             title
-          }
-          `,
+          }`,
   )
 }
 
 export async function getExperience(): Promise<Experience[]> {
   return client.fetch(
-    groq`*[_type == "experience"]{
-            _id,
-            title,
-            location,
-            description,
-            date,
-            icon
-        }`,
+    groq`*[_type == "experience"][0].expArrayField[]{
+      title,
+      location,
+      description,
+      date,
+      icon
+    }`,
   )
 }
