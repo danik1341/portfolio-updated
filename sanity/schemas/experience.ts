092f067d1e19c972cf96c9f0487f1e50705b30fc
danik1341 <danik1341@gmail.com>
2023-10-17 13:15:19 +0300

    schemas update, validation and sort

--- sanity/schemas/experience.ts
+++ sanity/schemas/experience.ts
@@ -4,36 +4,55 @@ export default {
   type: 'document',
   fields: [
     {
-      name: 'title',
-      type: 'string',
-      title: 'Title',
-    },
-    {
-      name: 'location',
-      type: 'string',
-      title: 'Location',
-    },
-    {
-      name: 'description',
-      type: 'text',
-      title: 'Description',
-    },
-    {
-      name: 'date',
-      type: 'string',
-      title: 'Date',
-    },
-    {
-      name: 'icon',
-      type: 'string',
-      title: 'Icon',
+      name: 'expArrayField',
+      title: 'Experiences Array',
+      type: 'array',
+      of: [
+        {
+          name: 'expObject',
+          title: 'Experience Object',
+          type: 'object',
+          fields: [
+            {
+              name: 'title',
+              type: 'string',
+              title: 'Title',
+            },
+            {
+              name: 'location',
+              type: 'string',
+              title: 'Location',
+            },
+            {
+              name: 'description',
+              type: 'text',
+              title: 'Description',
+            },
+            {
+              name: 'date',
+              type: 'string',
+              title: 'Date',
+            },
+            {
+              name: 'icon',
+              type: 'string',
+              title: 'Icon',
+              validation: (Rule: any) => Rule.required(),
+              options: {
+                list: [
+                  {title: 'Military', value: 'BsFillAirplaneEnginesFill'},
+                  {title: 'Education Cap', value: 'LuGraduationCap'},
+                  {title: 'Work', value: 'CgWorkAlt'},
+                  {title: 'React Education', value: 'FaReact'},
+                ],
+              },
+            },
+          ],
+        },
+      ],
       options: {
-        list: [
-          {title: 'Military', value: 'BsFillAirplaneEnginesFill'},
-          {title: 'Education Cap', value: 'LuGraduationCap'},
-          {title: 'Work', value: 'CgWorkAlt'},
-          {title: 'React Education', value: 'FaReact'},
-        ],
+        modal: 'popover',
+        sortable: true,
       },
     },
   ],
--- sanity/schemas/project.ts
+++ sanity/schemas/project.ts
@@ -1,47 +1,78 @@
 export default {
   name: 'project',
-  title: 'Project',
+  title: 'Projects',
   type: 'document',
   fields: [
     {
-      name: 'title',
-      type: 'string',
-      title: 'Title',
-    },
-    {
-      name: 'description',
-      type: 'text',
-      title: 'Description',
-    },
-    {
-      name: 'tags',
+      name: 'projectsArray',
+      title: 'Projects Array',
       type: 'array',
-      of: [{type: 'string'}],
-      title: 'Tags',
-    },
-    {
-      name: 'images',
-      type: 'array',
-      of: [{type: 'image'}],
-      title: 'Images',
-    },
-    {
-      name: 'slug',
-      type: 'slug',
-      title: 'Slug',
+      of: [
+        {
+          name: 'projectObject',
+          title: 'Project',
+          type: 'object',
+          fields: [
+            {
+              name: 'title',
+              type: 'string',
+              title: 'Title',
+            },
+            {
+              name: 'description',
+              type: 'text',
+              title: 'Description',
+            },
+            {
+              name: 'tags',
+              type: 'array',
+              validation: (Rule: any) => Rule.required(),
+
+              of: [{type: 'string'}],
+              title: 'Tags',
+            },
+            {
+              name: 'images',
+              validation: (Rule: any) => Rule.required(),
+
+              type: 'array',
+              of: [{type: 'image'}],
+              title: 'Images',
+            },
+            {
+              name: 'slug',
+              type: 'slug',
+              title: 'Slug',
+              validation: (Rule: any) => Rule.required(),
+              options: {
+                source: (doc: any) => {
+                  const title = doc.projectsArray[doc.projectsArray.length - 1].title
+                  const slug = title.toLowerCase().trim().replace(/\s+/g, '-').slice(0, 200)
+                  return slug
+                },
+                slugify: (input: string) =>
+                  input.toLowerCase().trim().replace(/\s+/g, '-').slice(0, 200),
+              },
+            },
+            {
+              name: 'githubLink',
+              type: 'url',
+              title: 'Github Link',
+              validation: (Rule: any) => Rule.required(),
+            },
+            {
+              name: 'demoLink',
+              type: 'url',
+              title: 'Demo Link',
+              validation: (Rule: any) => Rule.required(),
+            },
+          ],
+        },
+      ],
       options: {
-        source: 'title',
+        modal: 'popover',
+        sortable: true,
       },
-    },
-    {
-      name: 'githubLink',
-      type: 'url',
-      title: 'Github Link',
-    },
-    {
-      name: 'demoLink',
-      type: 'url',
-      title: 'Demo Link',
     },
   ],
 }
